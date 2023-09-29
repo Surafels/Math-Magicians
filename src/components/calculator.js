f5435ed4dd2357792e9ec8a4fe08a523c0aa99e1
Surafels <ssurafel4@gmail.com>
2023-09-29 14:11:32 +0300

    Fix errors

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -19,125 +19,27 @@ const Calculator = () => {
   return (
     <section>
       <div className="Container">
-        <InputText
-          type="text"
-          value={value.next || value.total || '0'}
-          className="output grid-element"
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="AC"
-          onClick={() => handleButtonClick('AC')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="+/-"
-          onClick={() => handleButtonClick('+/-')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="%"
-          onClick={() => handleButtonClick('%')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="÷"
-          onClick={() => handleButtonClick('÷')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="7"
-          onClick={() => handleButtonClick('7')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="8"
-          onClick={() => handleButtonClick('8')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="9"
-          onClick={() => handleButtonClick('9')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="x"
-          onClick={() => handleButtonClick('x')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="4"
-          onClick={() => handleButtonClick('4')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="5"
-          onClick={() => handleButtonClick('5')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="6"
-          onClick={() => handleButtonClick('6')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="-"
-          onClick={() => handleButtonClick('-')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="1"
-          onClick={() => handleButtonClick('1')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="2"
-          onClick={() => handleButtonClick('2')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="3"
-          onClick={() => handleButtonClick('3')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="+"
-          onClick={() => handleButtonClick('+')}
-        />
-        <InputButton
-          className="grid-element zero-button"
-          type="button"
-          value="0"
-          onClick={() => handleButtonClick('0')}
-        />
-        <InputButton
-          className="grid-element"
-          type="button"
-          value="."
-          onClick={() => handleButtonClick('.')}
-        />
-        <InputButton
-          className="grid-element equal-sign"
-          type="button"
-          value="="
-          onClick={() => handleButtonClick('=')}
-        />
+        <InputText type="text" value={value.next || value.total || '0'} className="output grid-element" />
+        <InputButton className="grid-element" type="button" value="AC" onClick={() => handleButtonClick('AC')} />
+        <InputButton className="grid-element" type="button" value="+/-" onClick={() => handleButtonClick('+/-')} />
+        <InputButton className="grid-element" type="button" value="%" onClick={() => handleButtonClick('%')} />
+        <InputButton className="grid-element operator" type="button" value="÷" onClick={() => handleButtonClick('÷')} />
+        <InputButton className="grid-element" type="button" value="7" onClick={() => handleButtonClick('7')} />
+        <InputButton className="grid-element" type="button" value="8" onClick={() => handleButtonClick('8')} />
+        <InputButton className="grid-element" type="button" value="9" onClick={() => handleButtonClick('9')} />
+        <InputButton className="grid-element operator" type="button" value="x" onClick={() => handleButtonClick('x')} />
+        <InputButton className="grid-element" type="button" value="4" onClick={() => handleButtonClick('4')} />
+        <InputButton className="grid-element" type="button" value="5" onClick={() => handleButtonClick('5')} />
+        <InputButton className="grid-element" type="button" value="6" onClick={() => handleButtonClick('6')} />
+        <InputButton className="grid-element operator" type="button" value="-" onClick={() => handleButtonClick('-')} />
+        <InputButton className="grid-element" type="button" value="1" onClick={() => handleButtonClick('1')} />
+        <InputButton className="grid-element" type="button" value="2" onClick={() => handleButtonClick('2')} />
+        <InputButton className="grid-element" type="button" value="3" onClick={() => handleButtonClick('3')} />
+        <InputButton className="grid-element operator" type="button" value="+" onClick={() => handleButtonClick('+')} />
+        <InputButton className="grid-element zero-button" type="button" value="0" onClick={() => handleButtonClick('0')} />
+        <InputButton className="grid-element" type="button" value="." onClick={() => handleButtonClick('.')} />
+        <InputButton className="grid-element equal-sign" type="button" value="=" onClick={() => handleButtonClick('=')} />
+
       </div>
     </section>
   );
