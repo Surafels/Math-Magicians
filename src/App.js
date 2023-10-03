0362f13c09d0034d3fb18c0b75a4ed6ec93684ac
Surafels <ssurafel4@gmail.com>
2023-10-03 13:15:43 +0300

    Add nav list and link the path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Quotes from './components/quotes';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <Quotes />
+    <>
+      <nav>
+        <p>Math Magicians</p>
+        <ul>
+          <li>
+            <Link to="/Home">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quotes">Quotes</Link>
+          </li>
 
-    </div>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/Home" index element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quotes" element={<Quotes />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,6 @@
+function Home() {
+  return (
+    <p>Hello</p>
+  );
+}
+export default Home;
