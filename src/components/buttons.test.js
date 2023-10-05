a2671300b4af6dbdf872e07025466a0febd2494b
FilbertKimasa <filbertdan67@gmail.com>
2023-10-05 16:12:26 +0300

    Add test for button.js

--- src/components/buttons.test.js
+++ src/components/buttons.test.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import InputButton from './button';
 
+test('Should render buttons correctly', () => {
+  const onClick = jest.fn();
+
+  const tree = renderer
+    .create(<InputButton value="Test" onClick={onClick} />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
