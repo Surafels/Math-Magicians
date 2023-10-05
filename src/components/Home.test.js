a29e7d8cd4a66b6ef94d9d4a88786cee04e41116
Surafels <ssurafel4@gmail.com>
2023-10-05 11:33:31 +0300

    Add test for home component using snapshot

--- src/components/Home.test.js
+++ src/components/Home.test.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import Home from './Home';
+import renderer from  'react-test-renderer';
+
+test('should render home div', () => {
+    const tree = renderer.create(<Home />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
